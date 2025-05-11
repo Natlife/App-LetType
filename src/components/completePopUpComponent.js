60c97648dafc7689333e0c0bc85397f1944cff2a
Ngo Duc Luong <ndluong1903@gmail.com>
2025-05-11 19:14:54 +0700

    feat:  stat-design

--- src/components/completePopUpComponent.js
+++ src/components/completePopUpComponent.js
@@ -19,15 +19,21 @@ class CompletePopUpComponent extends React.Component {
         return (
 
             <div>
-                <CongratComponent
+                {/* <CongratComponent
                     checkDoneTyping={this.props.checkDoneTyping}
-                />
+                /> */}
                 {this.props.checkDoneTyping &&
                     <>
                         <div className="popup-back"
                             onClick={() => this.props.handleClickButton(this.props.lengths)}
                         >
-                            <div className="notify-container"><h2>Enter to continue</h2></div>
+                            <div className="notify-stat">
+                                <div className="stat-container"><span className="stat-title">WPM:</span>  <span className="stat-value">100</span></div>
+                                <div className="stat-container"><span className="stat-title">ACC:</span>  <span className="stat-value">100%</span></div>
+                                <div className="stat-container"><span className="stat-title">Time:</span> <span className="stat-value">10:00</span></div>
+                            </div>
+                            <div className="notify-container"><div className="notify-continue">Enter to continue</div></div>
+                            <div className="notify-stat"></div>
                         </div>
                     </>
                 }
